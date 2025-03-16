8d41822412c62d3886fc8f386464ee51c5c10e2d
Ray-official-dev <raycast.var@gmail.com>
2025-03-16 17:32:26 +0200

    Added camera follow effect

---
+++ Assets/Game/Scripts/View/Utilits/CameraFollow/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Utilits
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform _target; // Об'єкт, за яким слідкує камера
+        [SerializeField] private float _smoothSpeed = 5f; // Швидкість згладжування
+
+        private void LateUpdate()
+        {
+            if (_target == null) 
+                return;
+
+            Vector3 targetPosition = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, _smoothSpeed * Time.deltaTime);
+        }
+
+        public void Set(Transform target)
+        {
+            _target = target;
+        }
+    }
+}
---
+++ Assets/Game/Scripts/View/Utilits/CameraFollow/CameraFollower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Utilits
+{
+    public class CameraFollower : MonoBehaviour
+    {
+        private void Start()
+        {
+            var cameraFollow = FindFirstObjectByType(typeof(CameraFollow)) as CameraFollow;
+
+            if (cameraFollow is null)
+                throw new System.Exception("Add follow camera to scene!");
+
+            cameraFollow.Set(gameObject.transform);
+        }
+    }
+}
